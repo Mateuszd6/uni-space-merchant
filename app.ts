0e89dac8580d0170e36f644565fde241e0cffaec
Mateusz Dudzinski <mateuszpd7@gmail.com>
2019-05-15 02:01:33 +0200

    Highscores.

--- app.ts
+++ app.ts
@@ -1,5 +1,47 @@
+export interface IScore {
+    name: string
+    score: number;
+}
+
 function myFunction() {
-    var playerNameInput = (<HTMLInputElement>document.getElementById("fname")).value;
+    let playerNameInput = (<HTMLInputElement>document.getElementById("fname")).value;
     sessionStorage.setItem("var_playerName", playerNameInput);
     sessionStorage.setItem("var_gameStarted", "session_OK");
+}
+
+window.onload = () => {
+    let highscoresJSON = localStorage.getItem("var_highscoresJSON");
+    let highscores = JSON.parse(highscoresJSON) as IScore[];
+    if (highscores == null)
+    {
+        highscores = [
+            { "name": "Bill Gates", "score": 2413 },
+            { "name": "Steve Jobs", "score": 2108 },
+            { "name": "Satya Nadella", "score": 2072 },
+            { "name": "Biarne Stroustroup", "score": 1998 },
+            { "name": "Steve Ballmer", "score": 1963 }
+        ];
+    }
+
+    console.log(highscores);
+    console.log(highscores.length);
+    highscores.sort((x, y) => y.score - x.score);
+    console.log(highscores);
+
+    localStorage.setItem("var_highscoresJSON", JSON.stringify(highscores));
+    console.log(localStorage.getItem("var_highscoresJSON"));
+
+    let list = document.getElementById("highscore-list");
+    let recordProto = list.querySelector("#highscore-record") as HTMLElement;
+    list.querySelectorAll("#highscore-record")
+        .forEach(x => { if (x != recordProto) x.remove(); });
+    recordProto.hidden = false;
+    highscores.forEach(function(x) {
+        let newRecord = recordProto.cloneNode(true) as HTMLElement;
+        newRecord.querySelector(".uname").textContent = x.name;
+        newRecord.querySelector(".score").textContent = x.score.toString();
+
+        list.append(newRecord);
+    });
+    recordProto.hidden = true;
 }
